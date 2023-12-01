115cf60d9720e2e7bd857e81ff6e537c4f9a28c0
sunjoolee <sheisthepoem@naver.com>
2023-12-01 14:56:21 +0900

    로또번호 생성기 UI

---
+++ app/src/main/java/com/sunjoolee/sparta_week1_lottogenerator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sunjoolee.sparta_week1_lottogenerator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
